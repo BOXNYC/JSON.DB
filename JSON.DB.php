c0751794b0d1a2d5150e59e5c13e14a3997b2460
Joseph James Weitzel <joe@box.biz>
2018-03-04 19:00:10 -0500

    Update JSON.DB.php

--- JSON.DB.php
+++ JSON.DB.php
@@ -1,12 +1,13 @@
 <?php
-if(isset($_POST['json'])):
-  $EPARATOR = '## DATABASE ## ?>';
+$PASSWORD = 'iheartny';
+$SEPARATOR = '## DATABASE ## ?>';
+if(isset($_POST['json']) && isset($_POST['pass']) && $_POST['pass'] == $PASSWORD):
   $new_json = json_encode($_POST['json']);
   if($new_json = json_decode($new_json)) {
     $current = file_get_contents(__FILE__);
-    $current_parts = explode($EPARATOR, $current);
+    $current_parts = explode($SEPARATOR, $current);
     if(count($current_parts) == 3)
-      file_put_contents(__FILE__, $current_parts[0] . $EPARATOR . $current_parts[1] . $EPARATOR . $_POST['json']);
+      file_put_contents(__FILE__, $current_parts[0] . $SEPARATOR . $current_parts[1] . $SEPARATOR . $_POST['json']);
   }
   header('Content-Type: application/json');
   print $_POST['json'];
