9fac0b599c95f7a614a13dc663d36c15a9b45e0f
Joseph James Weitzel <joe@box.biz>
2018-03-05 02:07:57 -0500

    Update JSON.DB.php

--- JSON.DB.php
+++ JSON.DB.php
@@ -3,9 +3,9 @@ $PASSWORD = 'CHANGE ME';
 $SEPARATOR = '## DATABASE ## ?>';
 
 $save = isset($_POST['json']);
-function exitMessage($success, $message){
+function exitMessage($success, $message, $message_key='message'){
   header('Content-Type: application/json');
-  print '{"success":'.($success?'true':'false').',"message":"'.$message.'"}';
+  print '{"success":'.($success?'true':'false').',"'.$message_key.'":'.$message.'}';
   exit;
 }
 if($save && $_POST['pass'] == $PASSWORD):
@@ -15,10 +15,10 @@ if($save && $_POST['pass'] == $PASSWORD):
     $current_parts = explode($SEPARATOR, $current);
     if(count($current_parts) == 3):
       file_put_contents(__FILE__, $current_parts[0] . $SEPARATOR . $current_parts[1] . $SEPARATOR . $_POST['json']);
-      exitMessage(TRUE, 'Saved!');
+      exitMessage(TRUE, '"Saved!"');
     endif;
   else:
-    exitMessage(FALSE, 'JSON parsing error.');
+    exitMessage(FALSE, '"JSON parsing error."');
   endif;
 elseif($save && isset($_POST['pass']) && $_POST['pass'] != $PASSWORD):
   header('Content-Type: application/json');
@@ -27,6 +27,15 @@ elseif($save && isset($_POST['pass']) && $_POST['pass'] != $PASSWORD):
 endif;
 if((isset($_GET['edit']) && (bool)$_GET['edit'] == TRUE) || (isset($_GET['edit']) && $_GET['edit'] == '')):
 echo file_get_contents('https://cdn.rawgit.com/BOXNYC/JSON.DB/master/assets/json.db.htm'); exit;
+elseif(isset($_GET['query']) && !empty($_GET['query'])):
+$file = 'http'.(isset($_SERVER['HTTPS'])?'s':'').'://'.$_SERVER['HTTP_HOST'].'/'.preg_replace('/\?.*$/','',$_SERVER['REQUEST_URI']);
+$JSON = file_get_contents($file, true);
+$JSON = json_decode($JSON, true);
+$code = explode('//', $_GET['query']);
+$code = '$JSON=$JSON'."['".implode("']['", $code)."'];";
+eval($code);
+if($JSON) exitMessage(TRUE, json_encode($JSON), 'data');
+else exitMessage(FALSE, '"Query retured null."');
 else: header('Content-Type: application/json'); endif;
 
 
